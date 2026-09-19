ada0ac1e1afc31984cb95b42fdb9f90aaa0aaec1
weizheIP <2106405330@qq.com>
2026-09-19 11:27:45 +0800

    Initial Commit

---
+++ manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  manager
+//
+//  Created by Zhuanz1 on 2026/9/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ manager/managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  managerApp.swift
+//  manager
+//
+//  Created by Zhuanz1 on 2026/9/19.
+//
+
+import SwiftUI
+
+@main
+struct managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
